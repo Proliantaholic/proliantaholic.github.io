5386d22000edbe8e4c1c81baf3a9a48bb29c9b78
Proliantaholic <49068319+Proliantaholic@users.noreply.github.com>
2020-10-04 15:09:28 +0800

    Add files via upload

--- AddNewIME.ps1
+++ AddNewIME.ps1
@@ -32,10 +32,10 @@ Set-WinUserLanguageList -LanguageList $UserLanguageList -Force
 if ([System.Environment]::OSVersion.Version.Build -ge 19041) {
     Write-Host "Set [Use previous version of] Microsoft Bopomofo, ChangJie and Quick IME (Windows 10 20H1 and above)..." -ForegroundColor Black -BackgroundColor Green
     Write-Host "Windows" ([System.Environment]::OSVersion.Version)
-    if((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}" -force -ea SilentlyContinue >$null 2>&1 }
-    if((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}" -force -ea SilentlyContinue >$null 2>&1 }
-    if((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{6024B45F-5C54-11D4-B921-0080C882687E}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{6024B45F-5C54-11D4-B921-0080C882687E}" -force -ea SilentlyContinue >$null 2>&1 }
-    if((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{B115690A-EA02-48D5-A231-E3578D2FDF80}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{B115690A-EA02-48D5-A231-E3578D2FDF80}" -force -ea SilentlyContinue >$null 2>&1 }
+    if ((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}" -force -ea SilentlyContinue >$null 2>&1 }
+    if ((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}" -force -ea SilentlyContinue >$null 2>&1 }
+    if ((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{6024B45F-5C54-11D4-B921-0080C882687E}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{6024B45F-5C54-11D4-B921-0080C882687E}" -force -ea SilentlyContinue >$null 2>&1 }
+    if ((Test-Path -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{B115690A-EA02-48D5-A231-E3578D2FDF80}") -ne $true) { New-Item "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{B115690A-EA02-48D5-A231-E3578D2FDF80}" -force -ea SilentlyContinue >$null 2>&1 }
     New-ItemProperty -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}" -Name "NoTsf3Override2" -Value 1 -PropertyType DWord -Force -ea SilentlyContinue >$null 2>&1
     New-ItemProperty -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}\{6024B45F-5C54-11D4-B921-0080C882687E}" -Name "NoTsf3Override2" -Value 1 -PropertyType DWord -Force -ea SilentlyContinue >$null 2>&1
     New-ItemProperty -LiteralPath "HKCU:\SOFTWARE\Microsoft\Input\TSF\Tsf3Override\{B115690A-EA02-48D5-A231-E3578D2FDF80}" -Name "NoTsf3Override4" -Value 1 -PropertyType DWord -Force -ea SilentlyContinue >$null 2>&1
@@ -43,8 +43,8 @@ if ([System.Environment]::OSVersion.Version.Build -ge 19041) {
 
 # Set Registry for adding New ChangJie and New Quick IME
 Write-Host "Set Registry for adding New ChangJie and New Quick IME..." -ForegroundColor Black -BackgroundColor Green
-if((Test-Path -LiteralPath $NewChangJieKey) -ne $true) { New-Item $NewChangJieKey -force -ea SilentlyContinue >$null 2>&1 }
-if((Test-Path -LiteralPath $NewQuickKey) -ne $true) { New-Item $NewQuickKey -force -ea SilentlyContinue >$null 2>&1 }
+if ((Test-Path -LiteralPath $NewChangJieKey) -ne $true) { New-Item $NewChangJieKey -force -ea SilentlyContinue >$null 2>&1 }
+if ((Test-Path -LiteralPath $NewQuickKey) -ne $true) { New-Item $NewQuickKey -force -ea SilentlyContinue >$null 2>&1 }
 New-ItemProperty -LiteralPath $NewChangJieKey -Name "Description" -Value "Microsoft New ChangJie" -PropertyType String -Force -ea SilentlyContinue >$null 2>&1
 New-ItemProperty -LiteralPath $NewChangJieKey -Name "Display Description" -Value "@%SystemRoot%\SYSTEM32\input.dll,-5093" -PropertyType ExpandString -Force -ea SilentlyContinue >$null 2>&1
 New-ItemProperty -LiteralPath $NewChangJieKey -Name "IconFile" -Value "%SystemRoot%\system32\IME\IMETC\ImTCTip.DLL" -PropertyType ExpandString -Force -ea SilentlyContinue >$null 2>&1
